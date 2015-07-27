fe132117b72c29fb8025ecd7ad5d790ef9aceda7
shandianwuzu <365663308@qq.com>
2015-07-26 18:15:39 -0700

    1

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,6 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
- 
+
 set :application, 'vps_test'
 set :repo_url, 'https://github.com/shandianwuzu/vps_test.git'
 
@@ -26,7 +26,7 @@ set :deploy_to, '/home/deploy/vps_test'
 # set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
- set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
